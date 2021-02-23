87ab4d4a4902976bb062169f984b6c7d2f5275b8
StopMakingUsernamesUnique <57193119+StopMakingUsernamesUnique@users.noreply.github.com>
2021-02-23 13:32:45 -0800

    Cleaned up Classes, Fixed Bugs, Added textGrabber
    array now grabs text from saved file before reading new characters, class is separated into methods single line text reading now bugless.

--- src/components/TextWatcher.ts
+++ src/components/TextWatcher.ts
@@ -1,29 +1,113 @@
 import * as vscode from 'vscode';
+import { resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
 
 export class TextWatcher{
+
+textGrabber(uri:vscode.Uri) {
+  var array: string[] = [''];
+
+  vscode.workspace.openTextDocument(uri).then((document) => {
+    var i:number = document.lineCount;
+      for(var a:number = 0; a < i; a++){
+        
+        array[a] = document.lineAt(a).text;
+
+    }
+    
+  });
+    
+  return array;
+}
+
+textWatcher(array:string[]){
+
+  let testing = vscode.window.createOutputChannel("Testing");
+
+  testing.appendLine("Start of Test------");
+  testing.show();
+
+  var string:string;
+
+  vscode.workspace.onDidChangeTextDocument(changeEvent => {
+                 
+              
+    for (const change of changeEvent.contentChanges) {   
+      if(change.range.end.character - change.range.start.character + 1 === change.text.length || change.text.length === 0){
+         testing.appendLine("start line:");
+         testing.appendLine(change.range.start.line.toString());
+         testing.appendLine("end line");
+         testing.appendLine(change.range.end.line.toString());
+         testing.appendLine("Start Char:");
+         testing.appendLine(change.range.start.character.toString()); 
+         testing.appendLine("End Char:");
+         testing.appendLine(change.range.end.character.toString()); 
+         testing.appendLine("length:");
+         testing.appendLine(change.text.length.toString());
+         testing.appendLine("text:");
+         testing.appendLine(change.text);  
+         testing.appendLine("Array:");
+         //Multiple Lines
+       /*  if(change.range.start.line < change.range.end.line && change.text.length === 0){
+           var lines = change.range.end.line - change.range.start.line;
+              for( var a = 0; a < lines; a++ ){
+                testing.appendLine("BcSPCML");
+            if(array[change.range.start.line + a ]){
+              string = array[change.range.start.line + a];
+              }
+              else{ string = '';}
+                 string = string.substr(0, change.range.start.character) + string.substr(change.range.end.character);   
+                  array[change.range.start.line + a] = string;
+                  string = '';
+              }
+              }
+          */
+         // Singular Line
+          if(change.text.length !== 0){
+            if(array[change.range.start.line]){
+            string = array[change.range.start.line];
+            }
+            else{ string = '';}
+            string = string.substr(0, change.range.start.character) + change.text + string.substr(change.range.end.character + change.text.length);
+              
+          array[change.range.start.line] = string;
+          string = '';
+          }
+
+          else{
+            testing.appendLine("BcSPC");
+            if(array[change.range.start.line]){
+              string = array[change.range.start.line];
+              }
+              else{ string = '';}
+            
+            string = string.substr(0, change.range.start.character) + string.substr(change.range.end.character);   
+            array[change.range.start.line] = string;
+            string = '';
+          }
+          testing.appendLine(array.toString());
+          
+          }
+     // 
+     // text replacement
+        }
+      }
+    
+    
+    );
+}
+
     constructor(){
 
-            var array: string[] = ["testStringggg"];
+            
 
-            let testing = vscode.window.createOutputChannel("Testing");
-            testing.appendLine("LIne");
-            testing.show();
-            vscode.workspace.onDidChangeTextDocument(changeEvent => {
-              // testing.appendLine(`Did change: ${changeEvent.document.uri}`);
+            //loader 
+            let uri = vscode.Uri.file('/Users/Idot/Documents/DualCoding-Example/words.txt');
+            var array:string[] = [''];
+            array = this.textGrabber(uri);
+            //Watcher
+            this.textWatcher(array);
 
-
-                for (const change of changeEvent.contentChanges) {
-                    if(change.range.start.character.toString() !== "0"){
-                     testing.appendLine(change.range.start.character.toString()); 
-                     testing.appendLine(change.text);  
-                     array[change.range.start.character, change.range.end.character] = change.text;
-                     testing.appendLine(array.toString());
-                    }
-                 // 
-                 // text replacement
-                
-                
-                }
-            });
+        
 }
+
 }
